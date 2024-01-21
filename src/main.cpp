61d4898a4f624374a3cff8bfc2b05bdca454137b
ragnarlodbrok1992 <maraus92@gmail.com>
2024-01-21 18:45:42 +0100

    WIP

--- src/main.cpp
+++ src/main.cpp
@@ -325,6 +325,30 @@ void Flush(Microsoft::WRL::ComPtr<ID3D12CommandQueue> commandQueue, Microsoft::W
   WaitForFenceValue(fence, fenceValueForSignal, fenceEvent);
 }
 
+// Update function for debug purposes from tutorial
+void Update() {
+  static uint64_t frameCounter = 0;
+  static double elapsedSeconds = 0.0;
+  static std::chrono::high_resolution_clock clock;
+  static auto t0 = clock.now();
+
+  frameCounter++;
+  auto t1 = clock.now();
+  auto deltaTime = t1 - t0;
+  t0 = t1;
+
+  elapsedSeconds += deltaTime.count() * 1e-9;
+  if (elapsedSeconds > 1.0) {
+    char buffer[500];
+    auto fps = frameCounter / elapsedSeconds;
+    sprintf_s(buffer, 500, "FPS: %f\n", fps);
+    OutputDebugStringA(buffer);
+
+    frameCounter = 0;
+    elapsedSeconds = 0.0;
+  }
+}
+
 // Debug function from tutorial
 void EnableDebugLayer() {
 #if defined(_DEBUG)
--- src/main.h
+++ src/main.h
@@ -5,6 +5,13 @@
 #define WIN32_LEAN_AND_MEAN
 #endif
 
+// Ancient macros undefined
+#ifdef max
+#undef max
+#endif
+
+#define NOMINMAX
+
 #include <windows.h>
 #include <shellapi.h>
 #include <wrl.h>
